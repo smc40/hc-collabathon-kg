828a22a0294bb28a23335f5ab459177c3091919f
Amanda M <amanda.mikelsons@hc-sc.gc.ca>
2024-11-06 15:54:24 -0500

    Getting small subset of nodes (hardcoded to 5 IDs)

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,6 @@ for n in unique_edge_nodes:
 st.title('Bob Blobs')
 
 for index, (source, target, weight) in df_edges.iterrows():
-    print(source + " " + target)
     edges.append(
         Edge(
             source=source,
